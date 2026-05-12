25c044b81cf0085f1f543e1abaa98a4d11a86d49
Samrah Khanzada <samrahsajidkhanzada@gmail.com>
2026-05-12 13:59:38 +0500

    Update API_URL for file sharing backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [file, setFile] = useState(null);
   const [files, setFiles] = useState([]);
   // const API_URL = "http://localhost:5000/api"; // Deployment ke waqt changehoga
-  const API_URL = "https://file-sharing-backend-z97w.vercel.app/api/files"; // Deployment ke waqt changehoga
+  const API_URL = "https://file-sharing-backend-z97w.vercel.app"; // Deployment ke waqt changehoga
   const fetchFiles = async () => {
     const res = await axios.get(`${API_URL}/files`);
     setFiles(res.data);
